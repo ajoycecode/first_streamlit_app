d8596437e4d7a5e603426efa0f45e099d3c2a332
A Joyce <107589463+ajoycecode@users.noreply.github.com>
2022-06-18 12:51:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.header('🍌🍓 Build Your own Fruit Smootie 🥝🍇')
 
 
 # Let's put apick list here so they can pick the fruit they want to include
-streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries]'])
+streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 
 streamlit.dataframe(my_fruit_list)
