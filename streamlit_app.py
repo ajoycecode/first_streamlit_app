7d695006dca4b671a960285112044b0f5d9abb60
A Joyce <107589463+ajoycecode@users.noreply.github.com>
2022-06-18 12:49:44 -0500

    Update streamlit_app.py
    prepicks for fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.header('🍌🍓 Build Your own Fruit Smootie 🥝🍇')
 
 
 # Let's put apick list here so they can pick the fruit they want to include
-streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberry'])
 
 
 streamlit.dataframe(my_fruit_list)
